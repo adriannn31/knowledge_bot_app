d3b5e7b427214163c38c7cd82aff068e9fad21bf
Adrian <adrianphang31@gmail.com>
2026-02-12 23:21:10 +0800

    Final App Build: Ready for Production

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3763075099"
+    serviceWorkerVersion: "1933406352"
   }
 });
